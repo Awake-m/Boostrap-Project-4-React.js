4d24a3a5efd1e2a42bd38fb9041aea04b0d9bdfd
Awake-m <jagrutimakwana@gmail.com>
2024-07-10 13:20:56 +0530

    bootstrap

---
+++ bootstrap/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Boostrap from './Componet/Boostrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Boostrap/>
+    </div>
+  );
+}
+
+export default App;
